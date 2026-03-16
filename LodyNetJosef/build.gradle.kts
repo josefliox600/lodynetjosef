a3a859ee1e9c99b2d9b27a7ee5aec6f227821d9d
josefliox600 <joseflioxi11@gmail.com>
2026-03-16 16:44:01 +0000

    Create build.gradle.kts

---
+++ LodyNetJosef/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    description = "Arabic content from LodyNet"
+    authors = listOf("Josef")
+
+    language = "ar"
+    status = 1
+
+    tvTypes = listOf(
+        "Movie",
+        "TvSeries",
+        "AsianDrama",
+        "Anime"
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=lodynet.watch&sz=%size%"
+}
