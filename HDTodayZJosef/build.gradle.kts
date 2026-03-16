7ade4199318d94cdecaaac31ab899fb926c61f47
josefliox600 <joseflioxi11@gmail.com>
2026-03-16 18:30:58 +0000

    Create build.gradle.kts

---
+++ HDTodayZJosef/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    authors = listOf("Josef")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=hdtodayz.to&sz=%size%"
+}
